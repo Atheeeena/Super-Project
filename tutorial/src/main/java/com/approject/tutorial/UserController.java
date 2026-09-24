41d02fb14202ef0d7b0721c249cbed3ebec54ceb
dkeen <dkeeney522@gmai..com>
2026-09-24 11:47:52 -0700

    partially finished UserController.java

--- tutorial/src/main/java/com/approject/tutorial/UserController.java
+++ tutorial/src/main/java/com/approject/tutorial/UserController.java
@@ -4,15 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
 
-    @GetMapping("/signup")
-    public String userAndPass(@RequestParam(name="user", required=false, defaultValue="World") String name, @RequestParam(name="pass", required=false, defaultValue="World") String password,Model model) {
-        model.addAttribute("name", name);
-        model.addAttribute("password", password);
-        return "signup";
+    @GetMapping("/pages/signuptrue.html")
+    @ResponseBody
+    public void userAndPass(@RequestParam(required = false) String user, @RequestParam(required = false) String pass) {
+        System.out.println("Username: " + user + "\nPassword: " + pass);
     }
 
 }
